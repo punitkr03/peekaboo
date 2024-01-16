e361735da8c0a4d24a814030ebbd2e2114ed5feb
Punit Kumar <tagpunitkumar@gmail.com>
2024-01-17 01:42:47 +0530

    pikapi

---
+++ index.js
@@ -0,0 +1,31 @@
+const yes = document.getElementById("yes");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const back = document.getElementById("back");
+const no = document.getElementById("no");
+const wrapper = document.querySelector('.wrapper');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = no.getBoundingClientRect();
+const decide = document.getElementById("decide-grp");
+
+const randomGenerator = () => {};
+
+yes.addEventListener("click", () => {
+  img1.style.display = "none";
+  img2.style.display = "block";
+  back.style.display = "block";
+  decide.style.display = "none";
+});
+
+back.addEventListener("click", () => {
+  window.location.reload();
+});
+
+no.addEventListener("mouseover", () => {
+  const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+  const j =
+    Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+  no.style.left = i + "px";
+  no.style.top = j + "px";
+  console.log(no)
+});
